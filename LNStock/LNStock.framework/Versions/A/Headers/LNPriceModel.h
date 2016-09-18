e488b28338e8b2801acef82f92e34a99119f96d9
wedxz <wedxzl@gmail.com>
2016-09-18 18:39:53 +0800

    add code

---
+++ LNStock/LNStock.framework/Versions/A/Headers/LNPriceModel.h
@@ -0,0 +1,13 @@
+//
+//  LNPriceModel.h
+//  LNStock
+//
+//  Created by vvusu on 9/7/16.
+//  Copyright © 2016 vvusu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LNPriceModel : NSObject
+
+@end
